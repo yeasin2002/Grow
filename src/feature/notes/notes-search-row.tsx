3d356fd95a7c6cc24cee6c11d8ed646a4890ae81
yeasin2002 <mdkawsarislam2002@gmail.com>
2026-05-02 17:09:14 +0600

    feat: added calender pages component

--- src/feature/notes/notes-search-row.tsx
+++ src/feature/notes/notes-search-row.tsx
@@ -1,17 +1,38 @@
 import { Input } from "heroui-native";
-import { Pressable, View } from "react-native";
+import { Pressable, StyleSheet, View } from "react-native";
 import { Icons } from "@/lib";
 
 export function NotesSearchRow() {
 	return (
 		<View className="mt-8 flex-row items-center gap-2">
-			<Input
-				placeholder="Find any note or documents"
-				className="flex-1 flex-row items-center rounded-2xl border border-[#dddddd] bg-transparent  py-8 min-h-12 pl-2 placeholder:pl-6"
-			/>
-			<Pressable className="size-12 items-center justify-center rounded-2xl bg-black active:opacity-90">
+			<Input placeholder="Find any note or documents" style={Styles.input} />
+			<Pressable style={Styles.button}>
 				<Icons className="text-white" name="add" size={34} />
 			</Pressable>
 		</View>
 	);
 }
+
+const Styles = StyleSheet.create({
+	input: {
+		flex: 1,
+		flexDirection: "row",
+		alignItems: "center",
+		borderRadius: 16,
+		borderWidth: 1,
+		borderColor: "#dddddd",
+		backgroundColor: "transparent",
+		paddingVertical: 32,
+		minHeight: 48,
+		paddingLeft: 8,
+	},
+
+	button: {
+		width: 48,
+		height: 48,
+		alignItems: "center",
+		justifyContent: "center",
+		borderRadius: 16,
+		backgroundColor: "#000",
+	},
+});
