68ca2adcac7409bf57f9d8e40bee51f6d6463c35
yeasin2002 <mdkawsarislam2002@gmail.com>
2026-05-13 16:42:51 +0600

    clear

--- apps/native/src/lib/icon-with-uniwind.tsx
+++
@@ -1,4 +0,0 @@
-import { Ionicons } from "@expo/vector-icons";
-
-import { withUniwind } from "uniwind";
-export const Icons = withUniwind(Ionicons);
--- packages/db/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { env } from "@grow/env/server";
-import { drizzle } from "drizzle-orm/node-postgres";
-
-import * as schema from "./schema/index";
-
-export const db = drizzle(env.DATABASE_URL, { schema });
