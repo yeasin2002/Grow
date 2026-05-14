39a300cc385172189327ab07b4eb4f497f5c5cca
yeasin2002 <mdkawsarislam2002@gmail.com>
2026-05-14 14:42:03 +0600

    updated lockfiles

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,15 +1,20 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withUniwindConfig } = require("uniwind/metro");
-const {
-  wrapWithReanimatedMetroConfig,
-} = require("react-native-reanimated/metro-config");
+const path = require("path");
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const projectRoot = __dirname;
+const monorepoRoot = path.resolve(projectRoot, "../..");
 
-const uniwindConfig = withUniwindConfig(wrapWithReanimatedMetroConfig(config), {
-  cssEntryFile: "./src/global.css",
-  dtsFile: "./uniwind-types.d.ts",
-});
+const config = getDefaultConfig(projectRoot);
 
-module.exports = uniwindConfig;
+config.watchFolders = [path.resolve(monorepoRoot, "node_modules/.pnpm")];
+
+config.resolver.unstable_enableSymlinks = true;
+config.resolver.unstable_enablePackageExports = true;
+
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(monorepoRoot, "node_modules"),
+  path.resolve(monorepoRoot, "node_modules/.pnpm/node_modules"),
+];
+
+module.exports = config;
