58f38a8e8d3fa325edf18847ee8d690f70b6c8bd
yeasin2002 <mdkawsarislam2002@gmail.com>
2026-05-14 19:32:45 +0600

    updated configs

---
+++ apps/mobile/src/lib/ts-rest.ts
@@ -0,0 +1,25 @@
+import { contract } from "@grow-final/api/index";
+import { env } from "@grow-final/env/native";
+import { QueryClient } from "@tanstack/react-query";
+import { initClient, tsRestFetchApi } from "@ts-rest/core";
+import { initTsrReactQuery } from "@ts-rest/react-query/v5";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 2,
+      staleTime: 1000 * 60,
+    },
+  },
+});
+
+const clientArgs = {
+  baseUrl: `${env.EXPO_PUBLIC_SERVER_URL}/rest`,
+  baseHeaders: {},
+  credentials: "include" as const,
+  api: tsRestFetchApi,
+};
+
+export const client = initClient(contract, clientArgs);
+
+export const tsr = initTsrReactQuery(contract, clientArgs);
