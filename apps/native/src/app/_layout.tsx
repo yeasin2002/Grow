558ed3c4febb27fd1df098c101468d229a0737cf
yeasin2002 <mdkawsarislam2002@gmail.com>
2026-05-19 16:47:27 +0600

    refactor: Remove root layout wrapper and simplify it.

--- apps/native/src/app/_layout.tsx
+++ apps/native/src/app/_layout.tsx
@@ -1,7 +1,12 @@
 import "@/global.css";
 import { Stack } from "expo-router";
 
-import { RootWrapper } from "@/components/common";
+import { AppThemeProvider } from "@/contexts/app-theme-context";
+import "@/global.css";
+import { HeroUINativeProvider } from "heroui-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { KeyboardProvider } from "react-native-keyboard-controller";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export const unstable_settings = {
 	initialRouteName: "(tabs)",
@@ -9,14 +14,24 @@ export const unstable_settings = {
 
 export default function StackLayout() {
 	return (
-		<RootWrapper>
-			<Stack
-				screenOptions={{
-					headerShown: false,
-					statusBarStyle: "dark",
-					statusBarTranslucent: true,
-				}}
-			/>
-		</RootWrapper>
+		<GestureHandlerRootView style={{ flex: 1 }}>
+			<SafeAreaProvider>
+				<KeyboardProvider>
+					<AppThemeProvider>
+						<HeroUINativeProvider
+							config={{ devInfo: { stylingPrinciples: false } }}
+						>
+							<Stack
+								screenOptions={{
+									headerShown: false,
+									statusBarStyle: "dark",
+									statusBarTranslucent: true,
+								}}
+							/>
+						</HeroUINativeProvider>
+					</AppThemeProvider>
+				</KeyboardProvider>
+			</SafeAreaProvider>
+		</GestureHandlerRootView>
 	);
 }
